fd5734fe34344486184a27b0fe82f7bb86522f6d
Lucas Emboava <emboavalucas@hotmail.com>
2024-06-04 23:12:42 -0300

    19th: validating user data

---
+++ auth/data/src/main/java/com/emboava/auth/data/EmailPatternValidator.kt
@@ -0,0 +1,10 @@
+package com.emboava.auth.data
+
+import android.util.Patterns
+import com.emboava.auth.domain.PatternValidator
+
+object EmailPatternValidator : PatternValidator {
+    override fun matches(value: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(value).matches()
+    }
+}
