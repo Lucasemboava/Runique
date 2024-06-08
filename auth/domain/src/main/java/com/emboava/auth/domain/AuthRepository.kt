5c4e16b230f8251d09dfbff843af277bdc0c1567
Lucas Emboava <emboavalucas@hotmail.com>
2024-06-07 22:39:55 -0300

    21th: registering users

---
+++ auth/domain/src/main/java/com/emboava/auth/domain/AuthRepository.kt
@@ -0,0 +1,8 @@
+package com.emboava.auth.domain
+
+import com.emboava.core.domain.util.DataError
+import com.emboava.core.domain.util.EmptyResult
+
+interface AuthRepository {
+    suspend fun register(email: String, password: String): EmptyResult<DataError.Network>
+}
---
+++ auth/presentation/src/main/java/com/emboava/auth/presentation/register/RegisterEvent.kt
@@ -0,0 +1,8 @@
+package com.emboava.auth.presentation.register
+
+import com.emboava.core.presentation.ui.UiText
+
+sealed interface RegisterEvent {
+    data object RegistrationSuccess : RegisterEvent
+    data class Error(val error: UiText) : RegisterEvent
+}
