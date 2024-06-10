1706618d32cd6d6e294eff6e304f747dad1b1dac
Lucas Emboava <emboavalucas@hotmail.com>
2024-06-09 22:01:35 -0300

    22th: setting up session storage

---
+++ core/data/src/main/java/com/emboava/core/data/auth/AuthInfoMapper.kt
@@ -0,0 +1,19 @@
+package com.emboava.core.data.auth
+
+import com.emboava.core.domain.AuthInfo
+
+fun AuthInfo.toAuthInfoSerializable(): AuthInfoSerializable {
+    return AuthInfoSerializable(
+        accessToken = accessToken,
+        refreshToken = refreshToken,
+        userId = userId
+    )
+}
+
+fun AuthInfoSerializable.toAuthInfo(): AuthInfo {
+    return AuthInfo(
+        accessToken = accessToken,
+        refreshToken = refreshToken,
+        userId = userId
+    )
+}
--- core/data/src/main/java/com/emboava/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/emboava/core/data/di/CoreDataModule.kt
@@ -1,10 +1,15 @@
 package com.emboava.core.data.di
 
+import com.emboava.core.data.auth.EncryptedSessionStorage
 import com.emboava.core.data.networking.HttpClientFactory
+import com.emboava.core.domain.SessionStorage
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
         HttpClientFactory().build()
     }
+    singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
