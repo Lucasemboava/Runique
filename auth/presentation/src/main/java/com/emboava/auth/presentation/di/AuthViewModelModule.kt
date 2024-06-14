12c13991582fc7ec7625805b8b0d0ddff9f912ef
Lucas Emboava <emboavalucas@hotmail.com>
2024-06-13 23:00:44 -0300

    25th: logging in users

--- auth/presentation/src/main/java/com/emboava/auth/presentation/di/AuthViewModelModule.kt
+++ auth/presentation/src/main/java/com/emboava/auth/presentation/di/AuthViewModelModule.kt
@@ -1,10 +1,12 @@
 package com.emboava.auth.presentation.di
 
 
+import com.emboava.auth.presentation.login.LoginViewModel
 import com.emboava.auth.presentation.register.RegisterViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val authViewModelModule = module {
     viewModelOf(::RegisterViewModel)
+    viewModelOf(::LoginViewModel)
 }
