1b73224779019577ac2599cae027f71247018a12
Lucas Emboava <emboavalucas@hotmail.com>
2024-07-22 18:51:43 -0300

    48th: implementing the logout mechanism

--- run/presentation/src/main/java/com/emboava/run/presentation/run_overview/RunOverviewViewModel.kt
+++ run/presentation/src/main/java/com/emboava/run/presentation/run_overview/RunOverviewViewModel.kt
@@ -5,9 +5,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.emboava.core.domain.SessionStorage
 import com.emboava.core.domain.run.RunRepository
 import com.emboava.core.domain.run.SyncRunScheduler
 import com.emboava.run.presentation.run_overview.mapper.toRunUi
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -15,7 +17,9 @@ import kotlin.time.Duration.Companion.minutes
 
 class RunOverviewViewModel(
     private val runRepository: RunRepository,
-    private val syncRunScheduler: SyncRunScheduler
+    private val syncRunScheduler: SyncRunScheduler,
+    private val applicationScope: CoroutineScope,
+    private val sessionStorage: SessionStorage
 ) : ViewModel() {
 
     var state by mutableStateOf(RunOverviewState())
@@ -40,15 +44,26 @@ class RunOverviewViewModel(
     }
 
     fun onAction(action: RunOverviewAction) {
-        when(action) {
-            RunOverviewAction.OnLogoutClick -> Unit
+        when (action) {
+            RunOverviewAction.OnLogoutClick -> logout()
             RunOverviewAction.OnStartClick -> Unit
             is RunOverviewAction.DeleteRun -> {
                 viewModelScope.launch {
                     runRepository.deleteRun(action.runUi.id)
                 }
             }
+
             else -> Unit
         }
+    }
+
+    private fun logout() {
+        applicationScope.launch {
+            syncRunScheduler.cancelAllSyncs()
+            runRepository.deleteAllRuns()
+            runRepository.logout()
+            sessionStorage.set(null)
+        }
+
     }
 }
