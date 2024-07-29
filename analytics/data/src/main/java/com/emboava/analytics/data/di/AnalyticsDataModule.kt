99389b7d1f283cb4014501cccf94cbfa558d6f45
Lucas Emboava <emboavalucas@hotmail.com>
2024-07-28 23:07:28 -0300

    52th: implementing dynamic delivery and upgrading to kotlin 2.0

--- analytics/data/src/main/java/com/emboava/analytics/data/di/AnalyticsDataModule.kt
+++ analytics/data/src/main/java/com/emboava/analytics/data/di/AnalyticsDataModule.kt
@@ -2,10 +2,14 @@ package com.emboava.analytics.data.di
 
 import com.emboava.analytics.data.RoomAnalyticsRepository
 import com.emboava.analytics.domain.AnalyticsRepository
+import com.emboava.core.database.RunDatabase
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val analyticsModule = module {
     singleOf(::RoomAnalyticsRepository).bind<AnalyticsRepository>()
+    single{
+        get<RunDatabase>().analyticsDao
+    }
 }
--- core/database/src/main/java/com/emboava/core/database/RunDatabase.kt
+++ core/database/src/main/java/com/emboava/core/database/RunDatabase.kt
@@ -2,6 +2,7 @@ package com.emboava.core.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.emboava.core.database.dao.AnalyticsDao
 import com.emboava.core.database.dao.RunDao
 import com.emboava.core.database.dao.RunPendingSyncDao
 import com.emboava.core.database.entity.DeletedRunSyncEntity
@@ -20,4 +21,5 @@ abstract class RunDatabase : RoomDatabase() {
 
     abstract val runDao: RunDao
     abstract val runPendingSyncDao: RunPendingSyncDao
+    abstract val analyticsDao: AnalyticsDao
 }
