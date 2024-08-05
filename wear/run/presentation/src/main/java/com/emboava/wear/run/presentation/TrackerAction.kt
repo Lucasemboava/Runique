2606064fc1744cba72d9b7f25c98464759c3793e
Lucas Emboava <emboavalucas@hotmail.com>
2024-08-04 22:12:30 -0300

    55th: implementing the tracker ui screen

---
+++ wear/run/presentation/src/main/java/com/emboava/wear/run/presentation/TrackerAction.kt
@@ -0,0 +1,6 @@
+package com.emboava.wear.run.presentation
+
+sealed interface TrackerAction {
+    data object OnToggleRunClick: TrackerAction
+    data object OnFinishRunClick: TrackerAction
+}
---
+++ wear/run/presentation/src/main/java/com/emboava/wear/run/presentation/di/RunPresentationModule.kt
@@ -0,0 +1,9 @@
+package com.emboava.wear.run.presentation.di
+
+import com.emboava.wear.run.presentation.TrackerViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.dsl.module
+
+val runPresentationModule = module {
+    viewModelOf(::TrackerViewModel)
+}
